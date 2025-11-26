872e4b32a98c815d11c9695c8031024d6c7c5389
Matthew Short <mshort@redhat.com>
2025-11-26 20:19:27 +0000

    Changed claude code install method from npm to native installer

--- post-create.sh
+++ post-create.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "Running post-create commands..."
 
-pip3 install --no-cache-dir pykeepass
-npm install -g @anthropic-ai/claude-code
+pip3 install --no-cache-dir pykeepass==4.1.1.post1
+# npm install -g @anthropic-ai/claude-code
+curl -fsSL https://claude.ai/install.sh | bash
+sudo mv ~/.local/bin/claude /usr/local/bin/
 python3 "${DEV_ENV_DIR}/general-setup.py"
 python3 "${DEV_ENV_DIR}/bootstrap-secrets.py"
 
