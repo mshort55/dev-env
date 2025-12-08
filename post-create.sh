2729e9960c32d5d1378de0636a9656ce079365a6
Matthew Short <mshort@redhat.com>
2025-12-08 21:23:27 +0000

    Removed bashrc source because it does not seem to be needed in post create script

--- post-create.sh
+++ post-create.sh
@@ -6,7 +6,7 @@ echo "Running post-create commands..."
 pip3 install --no-cache-dir -r "${DEV_ENV_DIR}/requirements.txt"
 # claude code install
 # shellcheck source=/dev/null
-echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.bashrc && source ~/.bashrc
+echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.bashrc
 curl -fsSL https://claude.ai/install.sh | bash
 python3 "${DEV_ENV_DIR}/general-setup.py"
 python3 "${DEV_ENV_DIR}/bootstrap-secrets.py"
